1ff880d22c86117059af7e683c649dc069ffa4e3
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-05-13 09:59:01 +0700

    deklarasi pointer

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -7,4 +7,7 @@ struct Node
     //deklarasi noMhs dan name untuk menampung data
     int noMhs;
     string name;
-}
+    //deklarassi pointer next dan pref untuk penujukan data sebelum dan sesudah
+    Node *next;
+    Node *prev;
+};

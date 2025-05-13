f3191b29377d2717d8192b971ba1bfb7e280b448
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-05-13 10:37:19 +0700

    step 1e - 6

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -61,5 +61,20 @@ void addNode()
 
         // Looping selama current != null dan noMhs dari current lebih kecil dari????
         while (current != NULL && current->noMhs < newNode->noMhs)
+        {                            //step 1.c: traverse the list to find the
+            previous = current;      //step 1.d: mode the previous to the current
+            current = current->next; //step 1.e: mode the current to the next
+
+        }
+
+        //set nilai next node baru = current dan prev node baru = previous
+        newNode->next = current;  //step 4: make the next field of the new nodes
+        newNode->prev = previous; //step 5: make the previous field of the new nodes
+
+        //kondisi jika current tidak sama dengan null
+        if (current != NULL)
+        {
+            current->prev = newNode; // step 6: make the previous field of the
+        }
     }
 }

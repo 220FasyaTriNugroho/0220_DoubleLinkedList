2aa8421e9a76755d5425f438c47a988a53eca201
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-05-13 10:03:40 +0700

    deklarasi prosedur addNode

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
 
-//membuat  struktur untuk object double linkedlist
+// membuat  struktur untuk object double linkedlist
 struct Node
 {
-    //deklarasi noMhs dan name untuk menampung data
+    // deklarasi noMhs dan name untuk menampung data
     int noMhs;
     string name;
-    //deklarassi pointer next dan pref untuk penujukan data sebelum dan sesudah
+    // deklarassi pointer next dan pref untuk penujukan data sebelum dan sesudah
     Node *next;
     Node *prev;
 };
 
 
-//deklarasi pointer START dan pemberian nilai
+// deklarasi pointer START dan pemberian nilai
 Node *START = NULL;
+
+//deklarasi prosedu addNode
+void addNode()
+{
+
+    
+}
